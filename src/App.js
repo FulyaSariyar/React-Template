20931d102a33456b40fbbf5f2741ceba09f73561
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2022-03-09 12:01:05 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import IfElseSample from "./jsxSample/IfElseSample";
+import JsxEvents from "./jsxSample/JsxEvents";
+import MapSample from "./jsxSample/MapSample";
+import Navbar from "./templateSample/Navbar";
+import Header from './templateSample/Header'
+import PageContent from "./templateSample/PageContent";
+import Footer from "./templateSample/Footer";
+
+function App() {
+
+
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Header></Header>
+      <PageContent></PageContent>
+      <Footer></Footer>
+    </>
+
+  );
+}
+
+export default App;
